caef2cf9cece36d54d65730f5e669faf28c38001
vijender kumar <vijenderkumargour>
2023-06-15 17:29:18 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOMv from "react-dom";
+import App from "./App";
+
+ReactDOMv.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
